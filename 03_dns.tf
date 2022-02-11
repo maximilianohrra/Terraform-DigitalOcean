384bc41185825d13c47344b5723c01a28de9365e
max : {25,11} <52332843+maximilianohrra@users.noreply.github.com>
2022-02-11 08:37:09 -0300

    Update 03_dns.tf

--- 03_dns.tf
+++ 03_dns.tf
@@ -1,14 +1,14 @@
 # Creamos un dominio nuevo
 
-resource "digitalocean_domain" "pablokbs" {
-  name = "pablokbs.com"
+resource "digitalocean_domain" "maxi" {
+  name = "maxi.com"
 }
 
 # Add a record to the domain
 resource "digitalocean_record" "www" {
-  domain = "${digitalocean_domain.pablokbs.name}"
+  domain = "${digitalocean_domain.maxi.name}"
   type   = "A"
-  name   = "pelado"
+  name   = "maxi"
   ttl    = "10"
   value  = "${digitalocean_droplet.web.ipv4_address}"
 }
